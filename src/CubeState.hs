b8a638ce6dc918cd48ae8f4b5a7bbbe7a3680f47
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-10-09 05:58:13 +0700

    reorganize import

--- src/CubeState.hs
+++ src/CubeState.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-module CubeState (stateFromCube, MatrixCube, CubeState, StateAI (..), CubeAI (..), GeneticAlgorithmAI(..)) where
+module CubeState (stateFromCube, MatrixCube, CubeState, StateAI (..), CubeAI (..), GeneticAlgorithmAI (..)) where
 
 import Control.Monad (foldM)
 import Control.Monad.IO.Class (MonadIO)
-import Data.Map (Map, fromList, fromListWith, (!))
+import Data.List (foldl')
+import qualified Data.Map as M
 import qualified Data.Set as S
-import GHC.Arr (Array, Ix (range), array, assocs, (!), (//), elems)
+import qualified GHC.Arr as A
 import Line (Line (..), Point (..), generateLines, lineToPoints)
 import System.Random (randomIO, randomRIO)
-import Data.List (foldl')
 
 type MatrixCube = [[[Int]]]
 
@@ -18,23 +18,23 @@ data CubeState = CubeState
     targetPoint :: Int,
     targetSum :: Int,
     currentPoint :: Int,
-    cube :: Array Point Int,
-    relatedLine :: Map Point [Line],
-    memoizedSum :: Map Line Int
+    cube :: A.Array Point Int,
+    relatedLine :: M.Map Point [Line],
+    memoizedSum :: M.Map Line Int
   }
   deriving (Show)
 
-generateRelatedLine :: [Line] -> Map Point [Line]
-generateRelatedLine ls = fromListWith (<>) t
+generateRelatedLine :: [Line] -> M.Map Point [Line]
+generateRelatedLine ls = M.fromListWith (<>) t
   where
     t = mconcat $ f <$> ls
     f l = [(p, [l]) | p <- lineToPoints l]
 
-runLine :: Array Point Int -> Line -> Int
-runLine c l = sum $ (c GHC.Arr.!) <$> lineToPoints l
+runLine :: A.Array Point Int -> Line -> Int
+runLine c l = sum $ (c A.!) <$> lineToPoints l
 
-generateMemoizedSum :: [Line] -> Array Point Int -> Map Line Int
-generateMemoizedSum ls c = fromList $ f <$> ls
+generateMemoizedSum :: [Line] -> A.Array Point Int -> M.Map Line Int
+generateMemoizedSum ls c = M.fromList $ f <$> ls
   where
     f l = (l, runLine c l)
 
@@ -56,7 +56,7 @@ stateFromCube s c =
     tp = length ls
     ms = generateMemoizedSum ls ar
     m = s - 1
-    ar = array (Point (0, 0, 0), Point (m, m, m)) $ do
+    ar = A.array (Point (0, 0, 0), Point (m, m, m)) $ do
       (z, l) <- zip [0 ..] c
       (y, l') <- zip [0 ..] l
       (x, e) <- zip [0 ..] l'
@@ -86,8 +86,8 @@ instance StateAI CubeState where
     where
       m = size s - 1
       r = do
-        p1@(Point (x1, y1, z1)) <- range (Point (0, 0, 0), Point (m, m, m))
-        p2 <- range (Point (x1, y1, z1), Point (m, m, m))
+        p1@(Point (x1, y1, z1)) <- A.range (Point (0, 0, 0), Point (m, m, m))
+        p2 <- A.range (Point (x1, y1, z1), Point (m, m, m))
         if p1 == p2
           then []
           else return $ switchValue s p1 p2
@@ -109,14 +109,14 @@ instance StateAI CubeState where
       else return $ switchValue s p1 p2
 
 instance CubeAI CubeState where
-  getValue s p = cube s GHC.Arr.! p
+  getValue s p = cube s A.! p
   setValue s p v' = ns
     where
-      v = cube s GHC.Arr.! p
+      v = cube s A.! p
       d = v' - v
-      rlm = f <$> relatedLine s Data.Map.! p
+      rlm = f <$> relatedLine s M.! p
         where
-          f l = (l, memoizedSum s Data.Map.! l)
+          f l = (l, memoizedSum s M.! l)
       nrlm = f <$> rlm
         where
           f (a, b) = (a, b + d)
@@ -125,8 +125,8 @@ instance CubeAI CubeState where
           f (_, b) r = r + if b == targetSum s then 1 else 0
       ns =
         s
-          { memoizedSum = fromList nrlm <> memoizedSum s,
-            cube = cube s // [(p, v')],
+          { memoizedSum = M.fromList nrlm <> memoizedSum s,
+            cube = cube s A.// [(p, v')],
             currentPoint = currentPoint s + countPoint nrlm - countPoint rlm
           }
   isMagicCube s = targetPoint s == currentPoint s
@@ -138,18 +138,20 @@ instance GeneticAlgorithmAI CubeState where
   combineGenes s1 s2 = do
     let z = zipWith f' (f s1) (f s2)
           where
-            f = assocs . cube
+            f = A.assocs . cube
             f' (p1, v1) (p2, _)
               | p1 /= p2 = error "Mismatch state"
               | otherwise = (p1, v1)
         fr f a b c = f c b a
     (p2, v2) <- fr foldM z ([], []) $ \a@(p2, v2) (p, v) -> do
       b <- randomIO :: IO Bool
-      if b then return a
-      else return (p:p2, v:v2)
+      if b
+        then return a
+        else return (p : p2, v : v2)
     let hv2 = S.fromList v2
-        s2v2 = foldl' f [] $ elems $ cube s2
-          where f a v = if v `S.member` hv2 then v:a else a
+        s2v2 = foldl' f [] $ A.elems $ cube s2
+          where
+            f a v = if v `S.member` hv2 then v : a else a
         t2 = p2 `zip` s2v2
         fc (p, v) a = setValue a p v
     return $ foldr fc s1 t2
