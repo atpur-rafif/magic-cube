603034df6c330003dfaf8c839a6ff579e71b51a9
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-11-03 09:19:58 +0700

    random population in genetic algorithm

--- src/Algorithm/GeneticAlgorithm.hs
+++ src/Algorithm/GeneticAlgorithm.hs
@@ -1,43 +1,44 @@
-{-# Language ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
+
 module Algorithm.GeneticAlgorithm where
 
 import Algorithm (Algorithm, iterateIO)
 import Control.Monad (forM)
 import qualified Control.Monad.Random as R
-import LocalSearch.State (State (nextRandomState, getPoint))
-import LocalSearch.Genetic(Genetic(combineGenes))
+import LocalSearch.Genetic (Genetic (combineGenes))
+import LocalSearch.State (State (getPoint, nextRandomState, randomState))
 import System.Random (randomIO)
+import Control.Monad.Random (replicateM)
 
 data Parameter = Parameter
   { maxIteration :: Int,
     populationSize :: Int
   }
 
--- TODO: Change replicate to random state
 run :: forall s. (State s, Genetic s) => Algorithm Parameter () s
-run a p s = g <$> iterateIO (maxIteration p, replicate (populationSize p) s) f
+run a p s = do
+  pp <- replicateM 10 $ randomState s
+  let f :: (Int, [s]) -> IO (Maybe (Int, [s]))
+      f (0, _) = return Nothing
+      f (i, ss) = do
+        a ()
+        let ssw = if all ((0 ==) . snd) l then fe <$> l else l
+              where
+                l = ft <$> ss
+                ft v = (v, toRational $ getPoint v)
+                fe (v, _) = (v, 1)
+            rn = R.fromList ssw
+        nss <- forM ss $ \_ -> do
+          p1 <- rn
+          p2 <- rn
+          m <- randomIO :: IO Bool
+          c <- combineGenes p1 p2
+          if m
+            then nextRandomState c
+            else return c
+        return $ Just (i - 1, nss)
+  ss <- iterateIO (maxIteration p, pp) f
+  return $ (foldr1 h . snd) ss
   where
-    f (0, _) = return Nothing
-    f (i, ss) = do
-      a ()
-      let ssw = if all ((0 ==) . snd) l then fe <$> l else l
-            where
-              l = ft <$> ss
-              ft v = (v, toRational $ getPoint v)
-              fe (v, _) = (v, 1)
-          rn = R.fromList ssw
-      nss <- forM ss $ \_ -> do
-        p1 <- rn
-        p2 <- rn
-        m <- randomIO :: IO Bool
-        c <- combineGenes p1 p2
-        if m
-          then nextRandomState c
-          else return c
-      return $ Just (i - 1, nss)
-
-    g :: (Int, [s]) -> s
-    g = foldr1 h . snd
-      where
-        h c r = if getPoint c > getPoint r then c else r
+    h c r = if getPoint c > getPoint r then c else r
